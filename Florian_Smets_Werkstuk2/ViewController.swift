e14d9fe22739a75b0bef86ce80678f58fba2d01e
student <student@students-mac.local>
2018-05-12 13:33:57 +0200

    Starten van tweede opdracht + joon parsen

--- Florian_Smets_Werkstuk2/ViewController.swift
+++ Florian_Smets_Werkstuk2/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+self.locationManager = CLLocationManager()
+self.locationManager.requestAlwaysAuthorization()
+self.locationManager.startUpdatingLocation()
+mapView.showsUserLocation = true
+        
+        struct Villo {
+            
+            
+            
+              }
+        
+        let url = URL(string: "http://opendatastore.brussels/dataset/villo")
+        let request = URLRequest(url: url!)
+        let session = URLSession(configuration:
+            URLSessionConfiguration.default)
+
+        let task = session.dataTask(with: request) {
+            (data, response, error) in
+            // check for errors
+            guard error == nil else {
+                print("error calling GET")
+                print(error!)
+                return
+            }
+            // make sure we got data
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+                
+                
+            }
+          
+
+         
+        }
+        task.resume()
+        
+        
+        
+        
+      
     }
 
     override func didReceiveMemoryWarning() {
